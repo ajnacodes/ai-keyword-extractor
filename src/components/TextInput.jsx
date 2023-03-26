5907527875a86224b837941338dfc62ce7b491f3
ajnacodes <b.daniela196@outlook.com>
2023-03-26 12:39:24 +0200

    extract keywords

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Textarea, Button, useToast } from "@chakra-ui/react";
 
-const TextInput = () => {
+const TextInput = ({ extractKeywords }) => {
   const [text, setText] = useState("");
 
   const toast = useToast();
@@ -16,7 +16,7 @@ const TextInput = () => {
         isClosable: false,
       });
     } else {
-      console.log(text);
+      extractKeywords(text);
     }
   };
   return (
