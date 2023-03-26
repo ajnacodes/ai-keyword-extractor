6a5cb495a4fdc45c2bbc26c3df2eef59cc57bebd
ajnacodes <b.daniela196@outlook.com>
2023-03-26 12:16:24 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
-        <h1>My app</h1>
+        <Header />
       </Container>
     </Box>
   );
