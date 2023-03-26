e82adc26e30e1cb14ab9638010b1341447279d50
ajnacodes <b.daniela196@outlook.com>
2023-03-26 13:20:57 +0200

    modal component  & spinner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TextInput from "./components/TextInput";
+import KeywordsModal from "./components/KeywordsModal";
 
 const App = () => {
   const [keywords, setKeywords] = useState("");
@@ -40,6 +41,9 @@ const App = () => {
     setLoading(false);
   };
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
   return (
     <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
@@ -47,6 +51,12 @@ const App = () => {
         <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   );
 };
