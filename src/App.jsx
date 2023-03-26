a6a430a358426281e8365f1e196e8e37fb5f648f
ajnacodes <b.daniela196@outlook.com>
2023-03-26 12:11:22 +0200

    base styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Container, Box } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>My app</h1>
-    </>
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <h1>My app</h1>
+      </Container>
+    </Box>
   );
 };
 
